7cad23d8e6bc8033b10af9e5159746d09c1c3773
meserkatie <37908336+meserkatie@users.noreply.github.com>
2018-04-22 20:08:31 -0500

    Add files via upload

---
+++ LoginActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Recommendr.Activities
+{ 
+    [Activity(Label = "Login to Account", MainLauncher = true, Icon = "@mipmap/ic_action_r")]
+    public class LoginActivity : Activity
+    {
+        //Step 2--Declare Class Variables
+        ImageView IVlogo;
+        ImageView IVlogin;
+        EditText txtUsername;
+        EditText txtPassword;
+        Button btnLogin;
+        Button btnSignUp;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            //Step 1--Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Login);
+
+            //Step 3--Find Controls
+            IVlogo = FindViewById<ImageView>(Resource.Id.IVlogo);
+            IVlogo.SetImageResource(Resource.Drawable.logo);
+            IVlogin = FindViewById<ImageView>(Resource.Id.IVlogin);
+            IVlogin.SetImageResource(Resource.Drawable.login5);
+            txtUsername = FindViewById<EditText>(Resource.Id.txtUsername);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+            btnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+
+            //Step 4--Event Handler(s)
+            btnSignUp.Click += BtnSignUp_Click;
+        }
+
+        private void BtnSignUp_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(SignUpActivity));
+        }
+    }
+}
---
+++ SignUpActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Recommendr.Activities
+{
+    [Activity(Label = "SignUpActivity")]
+    public class SignUpActivity : Activity
+    {
+        //Step 2--Declare Class Variables
+        ImageView IVlogo;
+        ImageView IVsignup;
+        EditText txtUsername;
+        EditText txtEmail;
+        EditText txtPassword;
+        EditText txtConfirmPassword;
+        Button btnSignUp;
+        Button btnLogin;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            //Step 1--Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.SignUp);
+
+            //Step 3--Find Controls
+            IVlogo = FindViewById<ImageView>(Resource.Id.IVlogo);
+            IVlogo.SetImageResource(Resource.Drawable.logo);
+            IVsignup = FindViewById<ImageView>(Resource.Id.IVSignUp);
+            IVsignup.SetImageResource(Resource.Drawable.createaccount2);
+            txtUsername = FindViewById<EditText>(Resource.Id.txtUsername);
+            txtEmail = FindViewById<EditText>(Resource.Id.txtEmail);
+            txtPassword = FindViewById<EditText>(Resource.Id.txtPassword);
+            txtConfirmPassword = FindViewById<EditText>(Resource.Id.txtConfirmPassword);
+            btnSignUp = FindViewById<Button>(Resource.Id.btnSignUp);
+            btnLogin = FindViewById<Button>(Resource.Id.btnLogin);
+
+            //Step 4--Event Handler(s)
+            btnLogin.Click += BtnLogin_Click;
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            StartActivity(typeof(LoginActivity));
+        }
+    }
+}
